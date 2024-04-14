f65b2941337f43d7d93a433d680a8d13277b02c4
karthikowo <20pw16@psgtech.ac.in>
2024-04-14 07:35:13 +0530

    add bs4 to req.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.stem import PorterStemmer
 from services.youtube_crawler_service import youtube_crawler_service
+import json
 
 def build_tf_idf(df):
     vectorizer = TfidfVectorizer()
@@ -35,6 +36,16 @@ def create_query_df(query_dict, idf):
 
 def modify_vector(df, i):
     return np.array(df.iloc[i]).reshape(1, -1)
+
+def like_click(query, title, id):
+    with open('user_interactions.txt', 'a') as f:
+        json.dump({'query': query, 'title': title, 'id': int(id), 'action': 'like'}, f)
+        f.write('\n')
+
+def dislike_click(query, title, id):
+    with open('user_interactions.txt', 'a') as f:
+        json.dump({'query': query, 'title': title, 'id': int(id), 'action': 'dislike'}, f)
+        f.write('\n')
 
 df = pd.read_csv('./irpackage.csv')
 df.dropna(inplace=True)
@@ -89,5 +100,5 @@ if st.button("Find the song", type='primary'):
         cdf = df.iloc[index]
         st.write(cdf)
         st.markdown(youtube_crawler_service.GetYtVideo(cdf['title']+' video song '+cdf['artist']), unsafe_allow_html=True)
-        st.button(label="\U0001f44d", key=f'like{i}')
-        st.button(label="\U0001f44e", key=f'dislike{i}')
+        st.button(label="\U0001f44d", key=f'like{i}', on_click= like_click, args=(query, cdf['title'], cdf.name))
+        st.button(label="\U0001f44e", key=f'dislike{i}', on_click= dislike_click,  args=(query, cdf['title'], cdf.name))
